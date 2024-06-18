b1faef6b4a5b755e591285bd17e9f98260b8a25b
nabielascar <ascarnabiel@gmail.com>
2024-06-18 14:29:54 +0700

    Menambahkan Login dan Register

---
+++ Tubes KPL GUI/Views/Register.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Views
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(423, 409);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 53);
+            button2.TabIndex = 34;
+            button2.Text = "Register";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 13);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 31);
+            button1.TabIndex = 33;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.CornflowerBlue;
+            label1.Location = new Point(350, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(265, 41);
+            label1.TabIndex = 26;
+            label1.Text = "Register Account ";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(281, 229);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(385, 27);
+            textBox2.TabIndex = 31;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(281, 309);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(385, 27);
+            textBox3.TabIndex = 32;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(281, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 20);
+            label3.TabIndex = 28;
+            label3.Text = "Username : ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(281, 146);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(385, 27);
+            textBox1.TabIndex = 30;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(281, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 27;
+            label2.Text = "Nama :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(281, 285);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 20);
+            label4.TabIndex = 29;
+            label4.Text = "Password";
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(942, 556);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label4);
+            Name = "Register";
+            Text = "Register";
+            Load += Register_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label4;
+    }
+}
---
+++ Tubes KPL GUI/Views/Register.cs
@@ -0,0 +1,127 @@
+﻿using Main.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Views
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Login _login = new Login();
+            string _username = textBox2.Text;
+            string _password = textBox3.Text;
+            string _role = "User";
+            string _nama = textBox1.Text;
+
+            List<Akun> _dataAkun = ReadJSON();
+
+
+            if (string.IsNullOrEmpty(_username) || string.IsNullOrEmpty(_password))
+            {
+                MessageBox.Show("Username dan Password tidak boleh kosong.");
+            }
+            else
+            {
+                Boolean _akunDitemukan = ValidateUser(_dataAkun, _username, _password);
+                if (_akunDitemukan)
+                {
+                    MessageBox.Show("Akun sudah ada");
+                }
+                else
+                {
+                    Boolean _statusPassword = ValidatePassword(_password);
+                    if (_statusPassword)
+                    {
+                        Akun newAkun = new Akun(_username, _password, _nama, _role);
+                        _dataAkun.Add(newAkun);
+                        WriteJSON(_dataAkun);
+                        MessageBox.Show("Akun berhasil dibuat");
+                        this.Dispose();
+                        _login.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password harus berisi lebih dari 6 dan kurang dari 10 karakter");
+                    }
+                }
+            }
+        }
+
+        private Boolean ValidateUser(List<Akun> newDataAkun, string username, string password)
+        {
+            for (int i = 0; i < newDataAkun.Count; i++)
+            {
+                if (newDataAkun[i].Password.Equals(password) && newDataAkun[i].Username.Equals(username))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Boolean ValidatePassword(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Akun> ReadJSON()
+        {
+            string filePathDataAkun = "C:\\Users\\nabiel\\Source\\Repos\\Tubes-KPL-GUI\\Tubes KPL GUI\\Main\\Data\\dataAkun.json";
+            List<Akun> dataAkun = new List<Akun>();
+            try
+            {
+                string configJsonData = File.ReadAllText(filePathDataAkun);
+                dataAkun = JsonSerializer.Deserialize<List<Akun>>(configJsonData);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            return dataAkun;
+
+        }
+        private void WriteJSON(List<Akun> newDataBarang)
+        {
+            string filePathDataAkun = "C:\\Users\\nabiel\\Source\\Repos\\Tubes-KPL-GUI\\Tubes KPL GUI\\Main\\Data\\dataAkun.json";
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(newDataBarang, options);
+            File.WriteAllText(filePathDataAkun, jsonString);
+        }
+    }
+}
+
