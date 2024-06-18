9ffce01cd13dbac987a48bd640e72fd9782fa00b
izharfahriansyah <izharfahriansyah7@gmail.com>
2024-06-18 13:17:28 +0700

    Adding Model View Controller

---
+++ Tubes KPL GUI/Main/Model/Hp.cs
@@ -0,0 +1,29 @@
+﻿namespace Main.Model
+{
+    public class Hp
+    {
+        public string NamaBarang { get; set; }
+        public int KodeBarang { get; set; }
+        public int JumlahBarang { get; set; }
+        public int HargaBarang { get; set; }
+
+        public Hp(string namaBarang, int kodeBarang, int jumlahBarang, int hargaBarang)
+        {
+            NamaBarang = namaBarang;
+            KodeBarang = kodeBarang;
+            JumlahBarang = jumlahBarang;
+            HargaBarang = hargaBarang;
+        }
+
+        internal static void Add(Hp Hp)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void RemoveAt(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
